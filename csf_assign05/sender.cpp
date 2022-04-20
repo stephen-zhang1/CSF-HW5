7fad16cb1ad3827eb4bfbe4fd59868cb17f6f9ac
Michael <menaye1@jhu.edu>
2022-04-19 21:33:56 -0400

    Implemented sender, hope it works

--- csf_assign05/sender.cpp
+++ csf_assign05/sender.cpp
@@ -22,11 +22,42 @@ int main(int argc, char **argv) {
   username = argv[3];
 
   // TODO: connect to server
+  Connection conn(); 
+  conn.connect(server_hostname, server_port);
+  // checks if connection is open
+  if (!conn.is_open) {
+    cerr << "Failed to connect to server\n";
+    return 2;
+  }
 
   // TODO: send slogin message
+  conn.send(TAG_SLOGIN);
+  conn.receive(TAG_OK);
+  conn.send(TAG_JOIN);
+  conn.receive(TAG_OK);
+
+  //declaring message formation objects
+  std::string data;
+  std::stringstream ss;
+  Message message;
+  bool done = false;
 
   // TODO: loop reading commands from user, sending messages to
   //       server as appropriate
+  while (!done) {
+    std::getline(std::cin, message);
+    ss << data;
+    message = new Message(username, message);
+    if (message == "/leave") {
+      done = true;
+    }
+    conn.send(message);
+    conn.receive(TAG_OK);
+  }
+
+  conn.send(TAG_QUIT);
+  conn.receive(TAG_OK);
+  conn.close();
 
   return 0;
 }
